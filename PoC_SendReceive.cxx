793c6d95fe0204c26f0b6f1986243a9ab77e49fb
Borja Outerelo <borjaouterelo@eprosima.com>
2017-09-14 11:03:40 +0200

    Send data payload instead of status

--- PoC_SendReceive.cxx
+++ PoC_SendReceive.cxx
@@ -48,30 +48,43 @@ size_t fill_buffer(octet* out_buffer, size_t buffer_len)
     XRCEFactory newMessage{reinterpret_cast<char*>(out_buffer), (uint32_t)buffer_len};
     newMessage.header(client_key, session_id, stream_id, sequence_nr);
 
-    OBJK_DATAWRITER_Status data_writer;
-    data_writer.stream_seq_num(0x7FFF);
-    data_writer.sample_seq_num(0x1234567890ABCDEF);
-    StatusVariant variant;
-    variant.data_writer(data_writer);
-    ResultStatus result;
-    result.request_id({ 0x10, 0x09, 0x08, 0x07 });
-    result.status(STATUS_OK);
-    result.implementation_status(STATUS_ERR_INCOMPATIBLE);
-    Status status;
-    status.status(variant);
-    status.result(result);
-    status.object_id({ 10,20,30 });
+    // OBJK_DATAWRITER_Status data_writer;
+    // data_writer.stream_seq_num(0x7FFF);
+    // data_writer.sample_seq_num(0x1234567890ABCDEF);
+    // StatusVariant variant;
+    // variant.data_writer(data_writer);
+    // ResultStatus result;
+    // result.request_id({ 0x10, 0x09, 0x08, 0x07 });
+    // result.status(STATUS_OK);
+    // result.implementation_status(STATUS_ERR_INCOMPATIBLE);
+    // Status status;
+    // status.status(variant);
+    // status.result(result);
+    // status.object_id({ 10,20,30 });
 
-    RESOURCE_STATUS_PAYLOAD resource_status;
-    resource_status.request_id({ 0x00, 0x01, 0x02, 0x03 });
-    resource_status.request_status(status);
+    // RESOURCE_STATUS_PAYLOAD resource_status;
+    // resource_status.request_id({ 0x00, 0x01, 0x02, 0x03 });
+    // resource_status.request_status(status);
 
-    SubmessageHeader submessage_header;
-    submessage_header.submessage_id(STATUS);
-    submessage_header.flags(0x07);
-    submessage_header.submessage_length(static_cast<uint16_t>(resource_status.getCdrSerializedSize(resource_status)));
+    // SubmessageHeader submessage_header;
+    // submessage_header.submessage_id(STATUS);
+    // submessage_header.flags(0x07);
+    // submessage_header.submessage_length(static_cast<uint16_t>(resource_status.getCdrSerializedSize(resource_status)));
 
-    newMessage.status(resource_status);
+    // newMessage.status(resource_status);
+
+    RT_Data data_reader;
+    SampleData sample_data;
+    sample_data.serialized_data({ 0x00,0x11,0x22, 0x33 });
+    data_reader.data(sample_data);
+    
+    DATA_PAYLOAD data;
+    data.data_reader(data_reader);
+    data.resource_id({ 0xF0,0xF1,0xF2 });
+    data.request_id({ 0x00,0x11,0x22, 0x33 });
+
+    newMessage.data(data);
+
     return newMessage.get_total_size();
 }
 
