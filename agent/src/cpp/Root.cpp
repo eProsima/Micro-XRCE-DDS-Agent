225b386596602bbd422792272fb9da3c50919b17
Borja Outerelo <borjaouterelo@eprosima.com>
2017-09-19 07:20:19 +0200

    Adds Apache License

--- agent/src/cpp/Root.cpp
+++ agent/src/cpp/Root.cpp
@@ -1,3 +1,17 @@
+// Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "agent/Root.h"
 
 #include "agent/Payloads.h"
@@ -123,6 +137,8 @@ void Agent::on_message(const MessageHeader& header, const SubmessageHeader& sub_
 
 void Agent::on_message(const MessageHeader& header, const SubmessageHeader& sub_header, const WRITE_DATA_PAYLOAD&  write_payload)
 {
+    client(id)->object(id)-cast->publisher->send
+    client(id)->send();
 }
 
 void Agent::on_message(const MessageHeader& header, const SubmessageHeader& sub_header, const READ_DATA_PAYLOAD&   read_payload)
