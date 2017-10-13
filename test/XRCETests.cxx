e64959ea9a7b4930362f2262804fa0a0b64ddbf2
Borja Outerelo <BorjaOuterelo@eprosima.com>
2017-10-13 09:57:50 +0200

    Adds override keyword

--- test/XRCETests.cxx
+++ test/XRCETests.cxx
@@ -342,19 +342,19 @@ class XRCEParserTests : public CommonData, public ::testing::Test
         }
 
         virtual void on_message(const MessageHeader& /*header*/, const SubmessageHeader& /*sub_header*/,
-                                const DELETE_RESOURCE_Payload& /*create_payload*/)
+                                const DELETE_RESOURCE_Payload& /*create_payload*/) override
         {
             // TODO write tests on this
         }
 
         virtual void on_message(const MessageHeader& /*header*/, const SubmessageHeader& /*sub_header*/,
-                                const WRITE_DATA_Payload& /*write_payload*/)
+                                const WRITE_DATA_Payload& /*write_payload*/) override
         {
             ++writes;
         }
 
         virtual void on_message(const MessageHeader& /*header*/, const SubmessageHeader& /*sub_header*/,
-                                const READ_DATA_Payload& /*read_payload*/)
+                                const READ_DATA_Payload& /*read_payload*/) override
         {
             ++reads;
         }
