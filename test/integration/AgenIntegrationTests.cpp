240bbb7bae6aabe7a1a6b88b777060523c541b01
Borja Outerelo <borjaouterelo@gmail.com>
2017-10-30 12:55:31 +0100

    Refactors tests. Creates new tests

--- test/integration/AgenIntegrationTests.cpp
+++ test/integration/AgenIntegrationTests.cpp
@@ -1,6 +1,8 @@
 #include "Common.h"
 
 #include <agent/Root.h>
+#include <agent/participant/Participant.h>
+
 #include <gtest/gtest.h>
 
 #include <thread>
@@ -20,60 +22,103 @@ class AgentTests : public ::testing::Test
 
     virtual ~AgentTests() = default;
 
+    ProxyClient* wait_client(ClientKey client_k, int trie_time, int max_tries)
+    {
+        ProxyClient* client = nullptr;
+        int count = 0;
+        do
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(trie_time));
+            client = agent_.get_client(client_k);
+        } while (client == nullptr && ++count < max_tries );        
+        EXPECT_LT (count, max_tries);  
+        return client; 
+    }
+
+    ProxyClient* wait_delete_client(ClientKey client_k, int trie_time, int max_tries)
+    {
+        ProxyClient* client = nullptr;
+        int count = 0;
+        do
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(trie_time));
+            client = agent_.get_client(client_k);
+        } while (client != nullptr && ++count < max_tries );        
+        EXPECT_LT (count, max_tries);  
+        return client; 
+    }
+
+    XRCEObject* wait_object(ProxyClient* client, const ObjectId& object_id, int trie_time, int max_tries)
+    {        
+        XRCEObject* object = nullptr;
+        if (client == nullptr)
+        {
+            return object;
+        }
+        int count = 0;
+        do
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(trie_time));
+            object = client->get_object(object_id);
+        } while (object == nullptr && ++count < max_tries );        
+        EXPECT_LT (count, max_tries);  
+        return object; 
+    }
+
+    XRCEObject* wait_delete_object(ProxyClient* client, const ObjectId& object_id, int trie_time, int max_tries)
+    {
+        XRCEObject* object = nullptr;
+        int count = 0;
+        do
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(trie_time));
+            object = client->get_object(object_id);
+        } while (object != nullptr && ++count < max_tries );       
+        EXPECT_LT (count, max_tries);  
+        return object; 
+    }
+
     eprosima::micrortps::Agent agent_;
     const ClientKey client_key = {{0xAA,0xBB,0xCC,0xDD}};
     std::thread agent_thread;
 };
 
-TEST_F(AgentTests, CreateClient)
+TEST_F(AgentTests, CreateDeleteClient)
 {
     const int trie_time = 1; // In seconds
     const int max_tries = 10;
     ASSERT_EQ(agent_.get_client(client_key), nullptr);
     agent_thread = std::thread(&Agent::run, &agent_);
-    ProxyClient* client = nullptr;
-    int count = 0;
-    do
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(trie_time));
-        client = agent_.get_client(client_key);
-    } while (client == nullptr && ++count < max_tries );        
-    EXPECT_LT (count, max_tries);
-    ProxyClient* delete_client = nullptr;
-    count = 0;
-    if (client != nullptr)
-    {
-        do
-        {
-            std::this_thread::sleep_for(std::chrono::seconds(trie_time));
-            delete_client = agent_.get_client(client_key);
-        } while (delete_client != nullptr && ++count < max_tries );
-        EXPECT_LT (count, max_tries);
-    }
+    ProxyClient* client = wait_client(client_key, trie_time, max_tries);
+    ProxyClient* delete_client = wait_delete_client(client_key, trie_time, max_tries);
     
     agent_.stop();
     agent_thread.join();  
     agent_.abort_execution();
     ASSERT_NE(client, nullptr);
-    ASSERT_EQ(delete_client, nullptr);    
+    ASSERT_EQ(delete_client, nullptr);
 }
 
-// TEST_F(AgentTests, CreateClient)
-// {
-//     ASSERT_EQ(agent_.get_client(client_key), nullptr);
-//     agent_thread = std::thread(&Agent::run, &agent_);
-//     ProxyClient* client = nullptr;
-//     int count = 0;
-//     do
-//     {
-//         std::this_thread::sleep_for(std::chrono::seconds(2));
-//         client = agent_.get_client(client_key);
-//     } while (client == nullptr && count++ < 5000000 );    
-//     agent_.stop();
-//     agent_thread.join();  
-//     agent_.abort_execution();
-//     EXPECT_NE(client, nullptr);
-// }
+TEST_F(AgentTests, CreateDeleteParticipant)
+{
+    const int trie_time = 1; // In seconds
+    const int max_tries = 10;
+    const ObjectId participant_id = {{0x00,0x01}};
+    ASSERT_EQ(agent_.get_client(client_key), nullptr);
+    agent_thread = std::thread(&Agent::run, &agent_);
+    ProxyClient* client = wait_client(client_key, trie_time, max_tries);
+    XRCEParticipant* participant = dynamic_cast<XRCEParticipant*>(wait_object(client, participant_id, trie_time, max_tries));
+    XRCEParticipant* delete_participant = dynamic_cast<XRCEParticipant*>(wait_delete_object(client, participant_id, trie_time, max_tries));
+    ProxyClient* delete_client = wait_delete_client(client_key, trie_time, max_tries);
+
+    agent_.stop();
+    agent_thread.join();  
+    agent_.abort_execution();
+    ASSERT_NE(client, nullptr);
+    ASSERT_NE(participant, nullptr);
+    ASSERT_EQ(delete_participant, nullptr);
+    ASSERT_EQ(delete_client, nullptr);
+}
 
 } // namespace testing
 } // namespace micrortps
