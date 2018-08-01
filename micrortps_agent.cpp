ba7bc74e09b215cfbce5022bbe2aa1f56091f9a3
julianbermudez <julianbermudez@eprosima.com>
2018-08-01 17:45:58 +0200

    Changed MicroRTPSAgent executable behaviour.

--- micrortps_agent.cpp
+++ micrortps_agent.cpp
@@ -161,11 +161,14 @@ int main(int argc, char** argv)
         std::cout << "    udp <local_port>" << std::endl;
         std::cout << "    tcp <local_port>" << std::endl;
     }
-
-    while (true)
+    else
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        while (true)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
     }
+
 
     return 0;
 }
