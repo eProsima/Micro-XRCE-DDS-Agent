34aff1c8794367935a766f03cf2b7b4d473f5e26
julianbermudez <julianbermudez@eprosima.com>
2018-09-12 09:27:55 +0200

    Refs #3379. Added entities destruction test.

--- test/unittest/root/RootUnitTests.cpp
+++ test/unittest/root/RootUnitTests.cpp
@@ -97,12 +97,107 @@ TEST_F(RootUnitTests, DeleteNoExistingClient)
 
     dds::xrce::CREATE_CLIENT_Payload create_data = generate_create_client_payload();
     dds::xrce::AGENT_Representation agent_representation;
-    dds::xrce::ResultStatus response      = root_.create_client(create_data.client_representation(),
-            agent_representation);
+    dds::xrce::ResultStatus response = root_.create_client(create_data.client_representation(), agent_representation);
     ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
 
     response = root_.delete_client(fake_client_key);
     ASSERT_EQ(dds::xrce::STATUS_ERR_UNKNOWN_REFERENCE, response.status());
+}
+
+TEST_F(RootUnitTests, CreateTree)
+{
+    /* Create client. */
+    dds::xrce::AGENT_Representation agent_representation;
+    dds::xrce::CLIENT_Representation client_representaiton = generate_create_client_payload().client_representation();
+    dds::xrce::ResultStatus response = root_.create_client(client_representaiton, agent_representation);
+    std::shared_ptr<ProxyClient> client = root_.get_client(client_representaiton.client_key());
+
+    /* Common creation mode. */
+    dds::xrce::CreationMode creation_mode;
+    creation_mode.reuse(false);
+    creation_mode.replace(true);
+
+    /*
+     * Create participant.
+     */
+    dds::xrce::ObjectVariant object_variant;
+    dds::xrce::OBJK_PARTICIPANT_Representation participant_representation;
+    participant_representation.domain_id(0);
+    participant_representation.representation().object_reference("default participant");
+    object_variant.participant(participant_representation);
+
+    dds::xrce::ObjectPrefix participant_id = {0x00, 0x01};
+    response = client->create(creation_mode, participant_id, object_variant);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
+
+    /*
+     * Create topic.
+     */
+    std::string topic_xml = "<profiles><topic><name>HelloWorldTopic</name><dataType>HelloWorld</dataType></topic></profiles>";
+    dds::xrce::OBJK_TOPIC_Representation topic_representation;
+    topic_representation.participant_id(participant_id);
+    topic_representation.representation().xml_string_representation(topic_xml);
+    object_variant.topic(topic_representation);
+
+    dds::xrce::ObjectPrefix topic_id = {0x00, 0x22};
+    response = client->create(creation_mode, topic_id, object_variant);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
+
+    /*
+     * Create publisher.
+     */
+    std::string publisher_xml = "";
+    dds::xrce::OBJK_PUBLISHER_Representation publisher_representation;
+    publisher_representation.participant_id(participant_id);
+    publisher_representation.representation().string_representation(publisher_xml);
+    object_variant.publisher(publisher_representation);
+
+    dds::xrce::ObjectPrefix publisher_id = {0x00, 0x13};
+    response = client->create(creation_mode, publisher_id, object_variant);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
+
+    /*
+     * Create datawriter.
+     */
+    std::string datawriter_xml = "<profiles><publisher profile_name=\"default_xrce_publisher_profile\"><topic><kind>NO_KEY</kind><name>HelloWorldTopic</name><dataType>HelloWorld</dataType><historyQos><kind>KEEP_LAST</kind><depth>5</depth></historyQos><durability><kind>TRANSIENT_LOCAL</kind></durability></topic></publisher></profiles>";
+    dds::xrce::DATAWRITER_Representation datawriter_representation;
+    datawriter_representation.publisher_id(publisher_id);
+    datawriter_representation.representation().string_representation(datawriter_xml);
+    object_variant.data_writer(datawriter_representation);
+
+    dds::xrce::ObjectPrefix datawriter_id = {0x00, 0x15};
+    response = client->create(creation_mode, datawriter_id, object_variant);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
+
+    /*
+     * Create subscriber.
+     */
+    std::string subscriber_xml = "";
+    dds::xrce::OBJK_SUBSCRIBER_Representation subscriber_representation;
+    subscriber_representation.participant_id(participant_id);
+    subscriber_representation.representation().string_representation(subscriber_xml);
+    object_variant.subscriber(subscriber_representation);
+
+    dds::xrce::ObjectPrefix subscriber_id = {0x00, 0x14};
+    response = client->create(creation_mode, subscriber_id, object_variant);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
+
+    /*
+     * Create datareader.
+     */
+    std::string datareader_xml = "<profiles><subscriber profile_name=\"default_xrce_subscriber_profile\"><topic><kind>NO_KEY</kind><name>HelloWorldTopic</name><dataType>HelloWorld</dataType><historyQos><kind>KEEP_LAST</kind><depth>5</depth></historyQos><durability><kind>TRANSIENT_LOCAL</kind></durability></topic></subscriber></profiles>";
+    dds::xrce::DATAREADER_Representation datareader_representation;
+    datareader_representation.subscriber_id(subscriber_id);
+    datareader_representation.representation().string_representation(datareader_xml);
+    object_variant.data_reader(datareader_representation);
+
+    dds::xrce::ObjectPrefix datareader_id = {0x00, 0x16};
+    response = client->create(creation_mode, datareader_id, object_variant);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
+
+    /* Participant destruction. */
+    response = client->delete_object(participant_id);
+    ASSERT_EQ(dds::xrce::STATUS_OK, response.status());
 }
 
 /*
