5a0da1605ab17f06a5e6e3fe8ed1f263519d3a69
julianbermudez <julianbermudez@eprosima.com>
2018-10-17 18:12:38 +0200

    Refs #3550. Taken common factor for client management in UDPServer.

--- src/cpp/transport/udp/UDPServerWindows.cpp
+++ src/cpp/transport/udp/UDPServerWindows.cpp
@@ -19,91 +19,10 @@ namespace eprosima {
 namespace uxr {
 
 UDPServer::UDPServer(uint16_t port)
-    : port_(port),
+    : UDPServerBase(port),
       poll_fd_{},
-      buffer_{0},
-      source_to_client_map_{},
-      client_to_source_map_{}
+      buffer_{0}
 {}
-
-void UDPServer::on_create_client(EndPoint* source, const dds::xrce::ClientKey& client_key)
-{
-    UDPEndPoint* endpoint = static_cast<UDPEndPoint*>(source);
-    uint64_t source_id = (uint64_t(endpoint->get_addr()) << 16) | endpoint->get_port();
-    uint32_t client_id = uint32_t(client_key.at(0) + (client_key.at(1) << 8) + (client_key.at(2) << 16) + (client_key.at(3) <<24));
-
-    /* Update maps. */
-    std::lock_guard<std::mutex> lock(clients_mtx_);
-    auto it_client = client_to_source_map_.find(client_id);
-    if (it_client != client_to_source_map_.end())
-    {
-        source_to_client_map_.erase(it_client->second);
-        it_client->second = source_id;
-    }
-    else
-    {
-        client_to_source_map_.insert(std::make_pair(client_id, source_id));
-    }
-
-    auto it_source = source_to_client_map_.find(source_id);
-    if (it_source != source_to_client_map_.end())
-    {
-        it_source->second = client_id;
-    }
-    else
-    {
-        source_to_client_map_.insert(std::make_pair(source_id, client_id));
-    }
-}
-
-void UDPServer::on_delete_client(EndPoint* source)
-{
-    UDPEndPoint* endpoint = static_cast<UDPEndPoint*>(source);
-    uint64_t source_id = (endpoint->get_addr() << 16) | endpoint->get_port();
-
-    /* Update maps. */
-    std::lock_guard<std::mutex> lock(clients_mtx_);
-    auto it = source_to_client_map_.find(source_id);
-    if (it != source_to_client_map_.end())
-    {
-        client_to_source_map_.erase(it->second);
-        source_to_client_map_.erase(it->first);
-    }
-}
-
-const dds::xrce::ClientKey UDPServer::get_client_key(EndPoint* source)
-{
-    dds::xrce::ClientKey client_key;
-    UDPEndPoint* endpoint = static_cast<UDPEndPoint*>(source);
-    std::lock_guard<std::mutex> lock(clients_mtx_);
-    auto it = source_to_client_map_.find((uint64_t(endpoint->get_addr()) << 16) | endpoint->get_port());
-    if (it != source_to_client_map_.end())
-    {
-        client_key.at(0) = uint8_t(it->second & 0x000000FF);
-        client_key.at(1) = uint8_t((it->second & 0x0000FF00) >> 8);
-        client_key.at(2) = uint8_t((it->second & 0x00FF0000) >> 16);
-        client_key.at(3) = uint8_t((it->second & 0xFF000000) >> 24);
-    }
-    else
-    {
-        client_key = dds::xrce::CLIENTKEY_INVALID;
-    }
-    return client_key;
-}
-
-std::unique_ptr<EndPoint> UDPServer::get_source(const dds::xrce::ClientKey& client_key)
-{
-    std::unique_ptr<EndPoint> source;
-    uint32_t client_id = uint32_t(client_key.at(0) + (client_key.at(1) << 8) + (client_key.at(2) << 16) + (client_key.at(3) <<24));
-    std::lock_guard<std::mutex> lock(clients_mtx_);
-    auto it = client_to_source_map_.find(client_id);
-    if (it != client_to_source_map_.end())
-    {
-        uint64_t source_id = it->second;
-        source.reset(new UDPEndPoint(uint32_t(source_id >> 16), uint16_t(source_id & 0xFFFF)));
-    }
-    return source;
-}
 
 bool UDPServer::init()
 {
