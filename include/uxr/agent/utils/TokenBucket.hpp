a1f5d95c8808f16223fa9c0ebc979d093e90b783
julianbermudez <julianbermudez@eprosima.com>
2019-04-02 14:37:24 +0200

    Refs #5405. Modify TokenBucket to header only class.

--- include/uxr/agent/utils/TokenBucket.hpp
+++ include/uxr/agent/utils/TokenBucket.hpp
@@ -25,17 +25,24 @@ namespace utils {
 class TokenBucket
 {
 public:
-    explicit TokenBucket(size_t rate, size_t burst = min_rate_);
-    TokenBucket(TokenBucket&& other) noexcept;
-    TokenBucket(const TokenBucket& other);
-    TokenBucket& operator=(TokenBucket&& other) noexcept;
-    TokenBucket& operator=(const TokenBucket& other);
-    ~TokenBucket()       = default;
+    explicit TokenBucket(
+            size_t rate,
+            size_t burst = min_rate_);
 
-    bool get_tokens(size_t tokens);
+    ~TokenBucket() = default;
+
+    TokenBucket(TokenBucket&& other) = default;
+    TokenBucket(const TokenBucket& other);
+    TokenBucket& operator=(TokenBucket&& other) = default;
+    TokenBucket& operator=(const TokenBucket& other);
+
+    std::chrono::milliseconds wait_time(size_t tokens_required);
+
+    bool get_tokens(size_t tokens_required);
 
 private:
-    size_t available_tokens();
+    void update_tokens();
+    size_t get_tokens_available();
 
 private:
     static constexpr size_t min_rate_ = 64000; // 64KB
@@ -44,8 +51,97 @@ private:
     size_t rate_;
     std::chrono::steady_clock::time_point timestamp_;
 
-    std::mutex data_mutex_;
+    std::mutex mtx_;
 };
+
+inline TokenBucket::TokenBucket(size_t rate, size_t burst)
+    : capacity_(burst),
+      rate_(rate)
+{
+    // Adjust to min rate
+    if (rate_ == 0)
+    {
+        rate_ = min_rate_;
+    }
+
+    // Adjust capacity to rate
+    if (rate_ > capacity_)
+    {
+        capacity_ = rate_;
+    }
+
+    // Adjust capacity to min rate if we use any kind of burst
+    if ((burst != 0) && (capacity_ < min_rate_))
+    {
+        capacity_ = min_rate_;
+    }
+
+    tokens_    = std::min(capacity_, rate_);
+    timestamp_ = std::chrono::steady_clock::now();
+}
+
+inline TokenBucket::TokenBucket::TokenBucket(const TokenBucket& other)
+    : capacity_(other.capacity_),
+      tokens_(other.tokens_),
+      rate_(other.rate_),
+      timestamp_(other.timestamp_)
+{
+}
+
+inline TokenBucket& TokenBucket::operator=(const TokenBucket& other)
+{
+    capacity_  = other.capacity_;
+    tokens_    = other.tokens_;
+    rate_      = other.rate_;
+    timestamp_ = other.timestamp_;
+    return *this;
+}
+
+inline std::chrono::milliseconds TokenBucket::wait_time(size_t tokens_required)
+{
+    std::chrono::milliseconds rv(0);
+    std::lock_guard<std::mutex> lock(mtx_);
+    size_t tokens_available = get_tokens_available();
+    if (tokens_required > tokens_available)
+    {
+        rv = std::chrono::milliseconds(
+                    std::max(uint64_t(1),
+                             uint64_t((std::milli::den * (tokens_available - tokens_required)) / rate_)));
+    }
+    return rv;
+}
+
+inline bool TokenBucket::get_tokens(size_t tokens_required)
+{
+    bool rv = false;
+    std::lock_guard<std::mutex> lock(mtx_);
+    if (tokens_required <= get_tokens_available())
+    {
+        update_tokens();
+        tokens_ -= tokens_required;
+        rv = true;
+    }
+    return rv;
+}
+
+inline void TokenBucket::update_tokens()
+{
+    auto current_time = std::chrono::steady_clock::now();
+    uint64_t delta_time =
+            uint64_t(std::chrono::duration_cast<std::chrono::milliseconds>(current_time - timestamp_).count());
+    tokens_ = std::min(capacity_, tokens_ + size_t((rate_ * delta_time) / std::milli::den));
+    timestamp_ = current_time;
+}
+
+inline size_t TokenBucket::get_tokens_available()
+{
+    auto current_time = std::chrono::steady_clock::now();
+    uint64_t delta_time =
+            uint64_t(std::chrono::duration_cast<std::chrono::milliseconds>(current_time - timestamp_).count());
+    return std::min(capacity_, tokens_ + size_t((rate_ * delta_time) / std::milli::den));
+}
+
+
 } // namespace utils
 } // namespace uxr
 } // namespace eprosima
