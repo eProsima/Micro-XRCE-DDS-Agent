0f9a5a23cdea7fd281bb694dfb658f079bb90c60
Julián Bermúdez Ortega <34604561+julianbermudez@users.noreply.github.com>
2019-05-29 16:46:21 +0200

    CedMiddleware documentation [4633] (#74)
    * Refs #4957. Add CedMiddleware comments.
    
    * Refs #4633. Attend pull request comments.

--- include/uxr/agent/middleware/ced/CedMiddleware.hpp
+++ include/uxr/agent/middleware/ced/CedMiddleware.hpp
@@ -29,123 +29,339 @@ public:
 
     ~CedMiddleware() override = default;
 
-/**********************************************************************************************************************
- * Create functions.
- **********************************************************************************************************************/
+    /**
+     * @startuml
+     * !include agent/middleware/ced/create_participant.puml!0
+     * @enduml
+     *
+     * @brief Creates a CedParticipant from a reference.
+     *        Currently, only the domain_id is taken into account for the creation.
+     *        In a near future, the Middleware interface should change the ref parameter by an "attributes" one.
+     *        For this purpose an XML parser and a reference data base are needed in the XRCE Core.
+     * @param participant_id    The CedParticipant identifier.
+     * @param domain_id         The Domain identifier to which the CedParticipant belongs.
+     * @param ref               NOT USED.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_participant_by_ref(
             uint16_t participant_id,
             int16_t domain_id,
-            const std::string& ref) override;
+            const std::string&) override;
 
+    /**
+     * @brief Creates a CedParticipant from an XML.
+     *        Currently, only the domain_id is taken into account for the creation.
+     *        In a near future, the Middleware interface should change the xml parameter by an "attributes" one.
+     *        For this purpose an XML parser and a reference data base are needed in the XRCE Core.
+     * @param participant_id    The CedParticipant identifier.
+     * @param domain_id         The Domain identifier to which the CedParticipant belongs.
+     * @param xml               NOT USED.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_participant_by_xml(
             uint16_t participant_id,
             int16_t domain_id,
-            const std::string& xml) override;
+            const std::string&) override;
 
+    /**
+     * @startuml
+     * !include agent/middleware/ced/create_topic.puml!0
+     * @enduml
+     *
+     * @brief Creates a CedTopic associated to a CedParticipant from a reference.
+     *        Currently, the ref paramenter is used as topic name.
+     *        In a near future, the Middleware interface should change the ref parameter by an "attributes" one.
+     *        For this purpose an XML parser and a reference data base are needed in the XRCE Core.
+     *        With the aforementioned modification the topic name will be extracted from the "attributes".
+     * @param topic_id          The CedTopic identifier.
+     * @param participant_id    The CedParticipant identifier to which the CedTopic is associated.
+     * @param ref               The CedTopic reference. Currently, it is used as the topic name.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_topic_by_ref(
             uint16_t topic_id,
             uint16_t participant_id,
             const std::string& ref) override;
 
+    /**
+     * @brief Creates a CedTopic associated to a CedParticipant from an XML.
+     *        Currently, the xml paramenter is used as topic name.
+     *        In a near future, the Middleware interface should change the xml parameter by an "attributes" one.
+     *        For this purpose an XML parser and a reference data base are needed in the XRCE Core.
+     *        With the aforementioned modification the topic name will be extracted from the "attributes".
+     * @param topic_id          The CedTopic identifier.
+     * @param participant_id    The CedParticipant identifier to which the CedTopic is associated.
+     * @param xml               The XML that describes the CedTopic. Currently, it is used as the topic name.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_topic_by_xml(
             uint16_t topic_id,
             uint16_t participant_id,
             const std::string& xml) override;
 
+    /**
+     * @startuml
+     * !include agent/middleware/ced/create_publisher.puml!0
+     * @enduml
+     *
+     * @brief Creates a CedPublisher associated to a CedParticipant.
+     * @param publisher_id      The CedPublisher identifier.
+     * @param participant_id    The CedParticipant identifier to which the CedPublisher is associated.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_publisher_by_xml(
             uint16_t publisher_id,
             uint16_t participant_id,
             const std::string&) override;
 
+    /**
+     * @startuml
+     * !include agent/middleware/ced/create_subscriber.puml!0
+     * @enduml
+     *
+     * @brief Creates a CedSubscriber associated to a CedParticipant.
+     * @param subscirber_id     The CedSubscriber identifier.
+     * @param participant_id    The CedParticipant identifier to which the CedSubscriber is associated.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_subscriber_by_xml(
             uint16_t subscirber_id,
             uint16_t participant_id,
             const std::string&) override;
 
+    /**
+     * @startuml
+     * !include agent/middleware/ced/create_datawriter.puml!0
+     * @enduml
+     *
+     * @brief Creates a CedDataWriter associated to a CedPublisher from a reference.
+     * @param datawriter_id         The CedDataWriter identifier.
+     * @param publisher_id          The CedPublisher identifier.
+     * @param ref                   The CedDataWriter reference. Currently, it is used as the topic name.
+     * @param associated_topic_id   The associated CedTopic identifier.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_datawriter_by_ref(
             uint16_t datawriter_id,
             uint16_t publisher_id,
             const std::string& ref,
             uint16_t& associated_topic_id) override;
 
+    /**
+     * @brief Creates a CedDataWriter associated to a CedPublisher from an XML.
+     * @param datawriter_id         The CedDataWriter identifier.
+     * @param publisher_id          The CedPublisher identifier.
+     * @param xml                   The XML that describes the CedDataWriter. Currently, it is used as the topic name.
+     * @param associated_topic_id   The associated CedTopic identifier.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_datawriter_by_xml(
             uint16_t datawriter_id,
             uint16_t publisher_id,
             const std::string& xml,
             uint16_t& associated_topic_id) override;
 
+    /**
+     * @startuml
+     * !include agent/middleware/ced/create_datareader.puml!0
+     * @enduml
+     *
+     * @brief Creates a CedDataReader associated to a CedSubscriber from a reference.
+     * @param datareader_id         The CedDataReader identifier.
+     * @param subscriber_id         The CedSubscriber identifier.
+     * @param ref                   The CedDataReader reference. Currently, it is used as the topic name.
+     * @param associated_topic_id   The associated CedTopic identifier.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_datareader_by_ref(
             uint16_t datareader_id,
             uint16_t subscriber_id,
             const std::string& ref,
             uint16_t& associated_topic_id) override;
 
+    /**
+     * @brief Creates a CedDataReader associated to a CedSubscriber from an XML.
+     * @param datareader_id         The CedDataReader identifier.
+     * @param subscriber_id         The CedSubscriber identifier.
+     * @param xml                   The XML that describes the CedDataReader. Currently, it is used as the topic name.
+     * @param associated_topic_id   The associated CedTopic identifier.
+     * @return  true in case of creation and false in other case.
+     */
     bool create_datareader_by_xml(
             uint16_t datareader_id,
             uint16_t subscriber_id,
             const std::string& xml,
             uint16_t& associated_topic_id) override;
 
-/**********************************************************************************************************************
- * Delete functions.
- **********************************************************************************************************************/
+    /**
+     * @brief Removes a CedParticipant from the participants register.
+     * @param participant_id    The CedParticipant identifier.
+     * @return  true in case of the CedParticipant was found and removed, false in other case.
+     */
     bool delete_participant(uint16_t participant_id) override;
 
+    /**
+     * @brief Removes a CedTopic from the topic register.
+     * @param topic_id  The CedTopic identifier.
+     * @return  true in case of the CedTopic was found and removed, false in other case.
+     */
     bool delete_topic(uint16_t topic_id) override;
 
+    /**
+     * @brief Removes a CedPublisher from the publisher register.
+     * @param publisher_id  The CedPublisher identifier.
+     * @return  true in case of the CedPublisher was found and removed, false in other case.
+     */
     bool delete_publisher(uint16_t publisher_id) override;
 
+    /**
+     * @brief Removes a CedSubscriber from the subscriber register.
+     * @param subscriber_id The CedSubscriber identifier.
+     * @return  true in case of the CedSubscriber was found and removed, false in other case.
+     */
     bool delete_subscriber(uint16_t subscriber_id) override;
 
+    /**
+     * @brief Removes a CedDataWriter from the datawriters register.
+     * @param datawriter_id The CedDataWriter identifier.
+     * @return  true in case of the CedDataWriter was found and removed, false in other case.
+     */
     bool delete_datawriter(uint16_t datawriter_id) override;
 
+    /**
+     * @brief Removes a CedDataReader from the datawriters register.
+     * @param datareader_id The CedDataReader identifier.
+     * @return  true in case of the CedDataReader was found and removed, false in other case.
+     */
     bool delete_datareader(uint16_t datareader_id) override;
 
-/**********************************************************************************************************************
- * Write/Read functions.
- **********************************************************************************************************************/
+    /**
+     * @brief Writes data using the CedDataWriter identified by the datawriter_id parameter.
+     * @param datawriter_id The CedDataWriter identifier.
+     * @param data          The data to be written.
+     * @return  true in case of successful writing and false in other case.
+     */
     bool write_data(
             uint16_t datawriter_id,
             const std::vector<uint8_t>& data) override;
 
+    /**
+     * @brief Read data using the CedDataReader identified by the datareader_id paramenter.
+     *        This is a blocking function that will block at most "timeout" milleseconds.
+     * @param datareader_id The CedDataReader's identifier.
+     * @param data          The data read.
+     * @param timeout       The timeout (milliseconds) of the reading.
+     * @return  true in case of successful reading and false in other case.
+     */
     bool read_data(
             uint16_t datareader_id,
             std::vector<uint8_t>& data,
             std::chrono::milliseconds timeout) override;
 
-/**********************************************************************************************************************
- * Matched functions.
- **********************************************************************************************************************/
+    /**
+     * @brief Checks whether an existing CedParticipant, identified by the participant_id, matches with a new
+     *        CedParticipant that would result from the creation of a new one using the domain_id and the reference
+     *        representation.
+     *        It is considered that the CedParticipants match if both have the same domain_id.
+     * @param participant_id    The existing CedParticipant identifier.
+     * @param domain_id         The Domain identifier of the new CedParticipant.
+     * @param ref               The reference that represents the new CedParticipant (unused).
+     * @return  true if both CedParticipant have the same Domain identifier, and false in other case.
+     */
     bool matched_participant_from_ref(
             uint16_t participant_id,
             int16_t domain_id,
             const std::string& ref) const override;
 
+    /**
+     * @brief Checks whether an existing CedParticipant, identified by the participant_id, matches with a new
+     *        CedParticipant that would result from the creation of a new one using the domain_id and the XML
+     *        representation.
+     *        It is considered that the CedParticipants match if both have the same domain_id.
+     * @param participant_id    The existing CedParticipant identifier.
+     * @param domain_id         The Domain identifier of the new CedParticipant.
+     * @param xml               The XML that describes the new CedParticipant (unused).
+     * @return  true if both CedParticipant have the same Domain identifier, and false in other case.
+     */
     bool matched_participant_from_xml(
             uint16_t participant_id,
             int16_t domain_id,
             const std::string& xml) const override;
 
+    /**
+     * @brief Checks whether an existing CedTopic, identified by the topic_id, matches with a new CedTopic that would
+     *        result from the creation of a new one using the reference representation.
+     *        It is considered that the CedTopics match if both have the same topic name.
+     * @param topic_id  The existing CedTopic identifier.
+     * @param ref       The reference that represents the new CedTopic.
+     *                  It defines the CedGlobalTopic name.
+     * @return  true if both CedTopic have associated the same CedGlobalTopic, and false in other case.
+     */
     bool matched_topic_from_ref(
             uint16_t topic_id,
             const std::string& ref) const override;
 
+    /**
+     * @brief Checks whether an existing CedTopic, identified by the topic_id, matches with a new CedTopic that would
+     *        result from the creation of a new one using the XML representation.
+     *        It is considered that the CedTopics match if both have the same topic name.
+     * @param topic_id  The existing CedTopic identifier.
+     * @param xml       The XML that describes the new CedTopic.
+     *                  It defines the CedGlobalTopic name.
+     * @return  true if both CedTopic have associated the same CedGlobalTopic, and false in other case.
+     */
     bool matched_topic_from_xml(
             uint16_t topic_id,
             const std::string& xml) const override;
 
+    /**
+     * @brief Checks whether an existing CedDataWriter, identified by the datawriter_id, matches with the new
+     *        CedDataWriter that would result from the creation of a new one using the reference representation.
+     *        It is considered that the CedDataWriters match if both are associated to the same CedGlobalTopic.
+     * @param datawriter_id The existing CedDataWriter identifier.
+     * @param ref           The reference that represents the new CedDataWriter.
+     *                      It defines the name of the associated CedGlobalTopic.
+     * @return true if both CedDataWriter have associated the same CedGlobalTopic name
+     */
     bool matched_datawriter_from_ref(
             uint16_t datawriter_id,
             const std::string& ref) const override;
 
+    /**
+     * @brief Checks whether an existing CedDataWriter, identified by the datawriter_id, matches with the new
+     *        CedDataWriter that would result from the creation of a new one using the XML representation.
+     *        It is considered that the CedDataWriters match if both are associated to the same CedGlobalTopic.
+     * @param datawriter_id The existing CedDataWriter identifier.
+     * @param ref           The reference that represents the new CedDataWriter.
+     *                      It defines the name of the associated CedGlobalTopic.
+     * @return true if both CedDataWriter have associated the same CedGlobalTopic name
+     */
     bool matched_datawriter_from_xml(
             uint16_t datawriter_id,
             const std::string& xml) const override;
 
+    /**
+     * @brief Checks whether an existing CedDataReader, identified by the datareader_id, matches with the new
+     *        CedDataReader that would result from the creation of a new one using the reference representation.
+     *        It is considered that the CedDataReaders match if both are associated to the same CedGlobalTopic.
+     * @param datareader_id The existing CedDataReader identifier.
+     * @param ref           The reference that represents the new CedDataReader.
+     *                      It defines the name of the associated CedGlobalTopic.
+     * @return true if both CedDataReader have associated the same CedGlobalTopic name
+     */
     bool matched_datareader_from_ref(
             uint16_t datareader_id,
             const std::string& ref) const override;
 
+    /**
+     * @brief Checks whether an existing CedDataReader, identified by the datareader_id, matches with the new
+     *        CedDataReader that would result from the creation of a new one using the XML representation.
+     *        It is considered that the CedDataReaders match if both are associated to the same CedGlobalTopic.
+     * @param datareader_id The existing CedDataReader identifier.
+     * @param ref           The reference that represents the new CedDataReader.
+     *                      It defines the name of the associated CedGlobalTopic.
+     * @return true if both CedDataReader have associated the same CedGlobalTopic name
+     */
     bool matched_datareader_from_xml(
             uint16_t datareader_id,
             const std::string& xml) const override;
