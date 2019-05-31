9bd380be5e495e8c61a515fc9b55be1c70ddaca4
julianbermudez <julianbermudez@eprosima.com>
2019-05-31 11:24:39 +0200

    Refs #5500. Remove eprosima_find_package approach.

--- cmake/SuperBuild.cmake
+++ cmake/SuperBuild.cmake
@@ -21,16 +21,18 @@ find_package(microxrcedds_client "1.0.1" EXACT QUIET)
 if(NOT microxrcedds_client_FOUND)
     ExternalProject_Add(uclient
         DOWNLOAD_COMMAND
-            git submodule update --init ${PROJECT_SOURCE_DIR}/thirdparty/uxrclient/
+            cd ${PROJECT_SOURCE_DIR}
+            git submodule update --init --recursive ${PROJECT_SOURCE_DIR}/thirdparty/uxrclient/
         PREFIX
             ${PROJECT_BINARY_DIR}/uclient
         SOURCE_DIR
-            ${PROJECT_SOURCE_DIR}/thirdparty/fastcdr
+            ${PROJECT_SOURCE_DIR}/thirdparty/uxrclient
         INSTALL_DIR
             ${PROJECT_BINARY_DIR}/temp_install
         CMAKE_ARGS
+            -DSUPERBUILD:BOOL=ON
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-            -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+            -DBUILD_SHARED_LIBS:BOOL=OFF
         )
     list(APPEND _deps uclient)
 endif()
@@ -40,6 +42,7 @@ find_package(fastcdr "1.0.8" EXACT QUIET)
 if(NOT microxrcedds_client_FOUND)
     ExternalProject_Add(fastcdr
         DOWNLOAD_COMMAND
+            cd ${PROJECT_SOURCE_DIR}
             git submodule update --init ${PROJECT_SOURCE_DIR}/thirdparty/fastcdr/
         PREFIX
             ${PROJECT_BINARY_DIR}/fastcdr
@@ -49,8 +52,7 @@ if(NOT microxrcedds_client_FOUND)
             ${PROJECT_BINARY_DIR}/temp_install
         CMAKE_ARGS
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-            -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
-            -DTHIRDPARTY:BOOL=ON
+            -DBUILD_SHARED_LIBS:BOOL=OFF
         )
     list(APPEND _deps fastcdr)
 endif()
@@ -60,6 +62,7 @@ find_package(fastrtps "1.7.2" EXACT QUIET)
 if(NOT microxrcedds_client_FOUND)
     ExternalProject_Add(fastrtps
         DOWNLOAD_COMMAND
+            cd ${PROJECT_SOURCE_DIR}
             git submodule update --init ${PROJECT_SOURCE_DIR}/thirdparty/fastrtps/
         PREFIX
             ${PROJECT_BINARY_DIR}/fastrtps
@@ -69,7 +72,7 @@ if(NOT microxrcedds_client_FOUND)
             ${PROJECT_BINARY_DIR}/temp_install
         CMAKE_ARGS
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-            -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+            -DBUILD_SHARED_LIBS:BOOL=OFF
             -DCMAKE_PREFIX_PATH:PATH="${CMAKE_PREFIX_PATH};${CMAKE_INSTALL_PREFIX}"
             -DTHIRDPARTY:BOOL=ON
         )
@@ -77,28 +80,56 @@ if(NOT microxrcedds_client_FOUND)
 endif()
 
 # CLI11.
-#find_package(CLI11 "1.7.1" EXACT QUIET)
-#if(NOT microxrcedds_client_FOUND)
-#    ExternalProject_Add(fastrtps
-#        DOWNLOAD_COMMAND
-#            git submodule update --init ${PROJECT_SOURCE_DIR}/thirdparty/fastrtps/
-#        PREFIX
-#            ${PROJECT_BINARY_DIR}/fastrtps
-#        SOURCE_DIR
-#            ${PROJECT_SOURCE_DIR}/thirdparty/fastrtps
-#        INSTALL_DIR
-#            ${PROJECT_BINARY_DIR}/temp_install
-#        CMAKE_ARGS
-#            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-#            -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
-#            -DCMAKE_PREFIX_PATH:PATH="${CMAKE_PREFIX_PATH};${CMAKE_INSTALL_PREFIX}"
-#            -DTHIRDPARTY:BOOL=ON
-#        )
-#    list(APPEND _deps fastrtps)
-#endif()
+find_package(CLI11 "1.7.1" EXACT QUIET)
+if(NOT microxrcedds_client_FOUND)
+    ExternalProject_Add(cli11
+        DOWNLOAD_COMMAND
+            cd ${PROJECT_SOURCE_DIR}
+            git submodule update --init ${PROJECT_SOURCE_DIR}/thirdparty/CLI11/
+        PREFIX
+            ${PROJECT_BINARY_DIR}/CLI11
+        SOURCE_DIR
+            ${PROJECT_SOURCE_DIR}/thirdparty/CLI11
+        INSTALL_DIR
+            ${PROJECT_BINARY_DIR}/temp_install
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DBUILD_SHARED_LIBS:BOOL=OFF
+            -DCMAKE_PREFIX_PATH:PATH="${CMAKE_PREFIX_PATH};${CMAKE_INSTALL_PREFIX}"
+            -DCLI11_TESTING:BOOL=OFF
+            -DCLI11_EXAMPLES:BOOL=OFF
+        )
+    list(APPEND _deps cli11)
+endif()
+
+# spdlog.
+find_package(spdlog "1.3.1" EXACT QUIET)
+if(NOT microxrcedds_client_FOUND)
+    ExternalProject_Add(spdlog
+        DOWNLOAD_COMMAND
+            cd ${PROJECT_SOURCE_DIR}
+            git submodule update --init ${PROJECT_SOURCE_DIR}/thirdparty/spdlog/
+        PREFIX
+            ${PROJECT_BINARY_DIR}/spdlog
+        SOURCE_DIR
+            ${PROJECT_SOURCE_DIR}/thirdparty/spdlog
+        INSTALL_DIR
+            ${PROJECT_BINARY_DIR}/temp_install
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DBUILD_SHARED_LIBS:BOOL=OFF
+            -DCMAKE_PREFIX_PATH:PATH="${CMAKE_PREFIX_PATH};${CMAKE_INSTALL_PREFIX}"
+            -DSPDLOG_BUILD_EXAMPLES:BOOL=OFF
+            -DSPDLOG_BUILD_TESTS:BOOL=OFF
+            -DSPDLOG_INSTALL:BOOL=ON
+        )
+    list(APPEND _deps spdlog)
+endif()
 
 # Main project.
 ExternalProject_Add(uagent
+    BUILD_ALWAYS
+        TRUE
     SOURCE_DIR
         ${PROJECT_SOURCE_DIR}
     BINARY_DIR
