668feff4c6b009c9fd8153c2222c42c821bea74e
Julián Bermúdez Ortega <julianbermudez@eprosima.com>
2020-03-06 11:53:27 +0100

    Refactor Serial transport protocol (#104)
    * Refs #5996. Refactor serial protocol transport.
    
    * Fix Windows warnings.

--- src/cpp/transport/serial/SerialAgentLinux.cpp
+++ src/cpp/transport/serial/SerialAgentLinux.cpp
@@ -29,7 +29,10 @@ SerialAgent::SerialAgent(
     , addr_{addr}
     , poll_fd_()
     , buffer_{0}
-    , serial_io_()
+    , serial_io_(
+          addr,
+          std::bind(&SerialAgent::write_data, this, std::placeholders::_1, std::placeholders::_2),
+          std::bind(&SerialAgent::read_data, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3))
     , errno_(0)
 {
     poll_fd_.fd = fd;
@@ -52,9 +55,6 @@ SerialAgent::~SerialAgent()
 
 bool SerialAgent::init()
 {
-    /* Init serial IO. */
-    uxr_init_serial_io(&serial_io_, addr_);
-
     /* Poll setup. */
     poll_fd_.events = POLLIN;
 
@@ -93,13 +93,11 @@ bool SerialAgent::close()
 }
 
 size_t SerialAgent::write_data(
-        void* instance,
         uint8_t* buf,
         size_t len)
 {
     size_t rv = 0;
-    SerialAgent* server = static_cast<SerialAgent*>(instance);
-    ssize_t bytes_written = ::write(server->poll_fd_.fd, (void*)buf, len);
+    ssize_t bytes_written = ::write(poll_fd_.fd, buf, len);
     if ((0 < bytes_written)  && size_t(bytes_written) == len)
     {
         rv = size_t(bytes_written);
@@ -108,17 +106,15 @@ size_t SerialAgent::write_data(
 }
 
 size_t SerialAgent::read_data(
-        void* instance,
         uint8_t* buf,
         size_t len,
         int timeout)
 {
     size_t rv = 0;
-    SerialAgent* server = static_cast<SerialAgent*>(instance);
-    int poll_rv = poll(&server->poll_fd_, 1, timeout);
+    int poll_rv = poll(&poll_fd_, 1, timeout);
     if (0 < poll_rv)
     {
-        ssize_t bytes_read = read(server->poll_fd_.fd, buf, len);
+        ssize_t bytes_read = read(poll_fd_.fd, buf, len);
         if (0 < bytes_read)
         {
             rv = size_t(bytes_read);
@@ -133,14 +129,7 @@ bool SerialAgent::recv_message(
 {
     bool rv = false;
     uint8_t remote_addr;
-    size_t bytes_read =
-            uxr_read_serial_msg(&serial_io_,
-                                read_data,
-                                this,
-                                buffer_,
-                                sizeof(buffer_),
-                                &remote_addr,
-                                timeout);
+    size_t bytes_read = serial_io_.read_msg(buffer_,sizeof (buffer_), remote_addr, timeout);
     if (0 < bytes_read)
     {
         input_packet.message.reset(new InputMessage(buffer_, static_cast<size_t>(bytes_read)));
@@ -169,12 +158,10 @@ bool SerialAgent::send_message(
 {
     bool rv = false;
     size_t bytes_written =
-            uxr_write_serial_msg(&serial_io_,
-                                 write_data,
-                                 this,
-                                 output_packet.message->get_buf(),
-                                 output_packet.message->get_len(),
-                                 output_packet.destination.get_addr());
+            serial_io_.write_msg(
+                output_packet.message->get_buf(),
+                output_packet.message->get_len(),
+                output_packet.destination.get_addr());
     if ((0 < bytes_written) && (bytes_written == output_packet.message->get_len()))
     {
         rv = true;
---
+++ src/cpp/transport/serial/SerialProtocol.cpp
@@ -0,0 +1,9 @@
+#include <uxr/agent/transport/serial/SerialProtocol.hpp>
+
+namespace eprosima {
+namespace uxr {
+
+constexpr uint16_t SerialIO::crc16_table[256];
+
+} // namespace uxr
+} // namespace eprosima
