ff5c13737d65501fed142f4edf5444f7523c4815
Pablo Garrido <pablogs9@gmail.com>
2020-06-01 15:44:18 +0200

    Change FastRTPS name to FastDDS

--- cmake/SuperBuild.cmake
+++ cmake/SuperBuild.cmake
@@ -101,19 +101,19 @@ if(UAGENT_FAST_PROFILE)
             )
     endif()
 
-    # Fast RTPS.
-    unset(fastrtps_DIR CACHE)
-    find_package(fastrtps ${_fastrtps_version} EXACT QUIET)
-    if(NOT fastrtps_FOUND)
-        ExternalProject_Add(fastrtps
+    # Fast DDS.
+    unset(fastdds_DIR CACHE)
+    find_package(fastdds ${_fastdds_version} EXACT QUIET)
+    if(NOT fastdds_FOUND)
+        ExternalProject_Add(fastdds
             GIT_REPOSITORY
-                https://github.com/eProsima/Fast-RTPS.git
+                https://github.com/eProsima/Fast-DDS.git
             GIT_TAG
-                ${_fastrtps_tag}
+                ${_fastdds_tag}
             PREFIX
-                ${PROJECT_BINARY_DIR}/fastrtps
+                ${PROJECT_BINARY_DIR}/fastdds
             INSTALL_DIR
-                ${PROJECT_BINARY_DIR}/temp_install/fastrtps-${_fastrtps_version}
+                ${PROJECT_BINARY_DIR}/temp_install/fastrtps-${_fastdds_version}
             CMAKE_CACHE_ARGS
                 -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
                 -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH};${PROJECT_BINARY_DIR}/temp_install
@@ -134,7 +134,7 @@ if(UAGENT_FAST_PROFILE)
             TEST_COMMAND
                 COMMAND ${CMAKE_COMMAND} -E rename <SOURCE_DIR>/src/cpp/CMakeLists.txt.bak <SOURCE_DIR>/src/cpp/CMakeLists.txt
             )
-        list(APPEND _deps fastrtps)
+        list(APPEND _deps fastdds)
     endif()
 endif()
 
