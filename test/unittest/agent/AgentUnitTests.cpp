c00bd1c49e256b9799dee93b5ebba12ecc74a2e1
Ricardo González <ricardo@richiware.dev>
2022-12-09 11:02:35 +0100

    Increase cmake minimum required according to fastdds (#323)
    * Refs #16427. Increase cmake minimum required according to fastdds
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #16427. Fix googletest warning
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Set client develop branch
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>

--- test/unittest/agent/AgentUnitTests.cpp
+++ test/unittest/agent/AgentUnitTests.cpp
@@ -1077,7 +1077,13 @@ TEST_P(AgentUnitTests, RegisterCallbackFunctions)
 
 // TODO (pablogs): Add tests for binary entity creation
 
-INSTANTIATE_TEST_CASE_P(AgentUnitTestsParams,
+#ifdef INSTANTIATE_TEST_SUITE_P
+#define GTEST_INSTANTIATE_TEST_MACRO(x, y, z) INSTANTIATE_TEST_SUITE_P(x, y, z)
+#else
+#define GTEST_INSTANTIATE_TEST_MACRO(x, y, z) INSTANTIATE_TEST_CASE_P(x, y, z)
+#endif // ifdef INSTANTIATE_TEST_SUITE_P
+
+GTEST_INSTANTIATE_TEST_MACRO(AgentUnitTestsParams,
                         AgentUnitTests,
                         ::testing::Values(Middleware::Kind::FASTRTPS,Middleware::Kind::FASTDDS));
 
